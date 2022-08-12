53f7fe7fc6cdafae409cd7e33402a8c0eb6a6a68
Infinite Aengus <infiniteaengus@gmail.com>
2022-08-12 14:06:35 -0400

    update deploy command

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config({ debug: process.env.DEBUG });
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -15,8 +15,8 @@ for (const file of commandFiles) {
 
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
